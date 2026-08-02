87fa172da35333ed8a5334d7fb394c19af436cfb
MiguelD <migueldoliveira142@gmail.com>
2026-08-02 07:31:30 +0100

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   mode: "development",
@@ -8,4 +9,9 @@ export default {
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
