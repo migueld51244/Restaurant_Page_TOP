3079dc1c277eca1d5378b72108fd90bac283c524
MiguelD <migueldoliveira142@gmail.com>
2026-08-10 16:29:16 +0100

    Added title

--- src/modules/about.js
+++ src/modules/about.js
@@ -2,6 +2,12 @@ const buildAboutSection = () => {
   const aboutContainer = document.createElement("div");
   aboutContainer.classList.add("about-container");
 
+  // About section title
+  const aboutPageTitle = document.createElement("h2");
+  aboutPageTitle.innerText = "About Us";
+  aboutPageTitle.classList.add("about-page-title");
+  aboutContainer.appendChild(aboutPageTitle);
+
   return aboutContainer;
 };
 
