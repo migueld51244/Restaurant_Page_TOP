162aef63e4fc5a05894627c6958a741d8ccf0d3c
MiguelD <migueldoliveira142@gmail.com>
2026-08-10 16:17:32 +0100

    Created basic about section

--- src/modules/about.js
+++ src/modules/about.js
@@ -1,3 +1,8 @@
-const buildAboutSection = () => {};
+const buildAboutSection = () => {
+  const aboutContainer = document.createElement("div");
+  aboutContainer.classList.add("about-container");
+
+  return aboutContainer;
+};
 
 export default buildAboutSection;
