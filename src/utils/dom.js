6c47588bfa891fd61d45ee6d17cbc55a22e0f83b
MiguelD <migueldoliveira142@gmail.com>
2026-08-11 16:10:53 +0100

    Changed element paramenter name

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,6 +1,6 @@
-export function createEl(element, cls, text) {
-  const element = document.createElement(element);
-  if(cls) element.classList.add(cls);
-  if(text) element.textContent = text;
-  return element;
+export function createEl(el, cls, text) {
+  const element = document.createElement(el);
+  if(cls) el.classList.add(cls);
+  if(text) el.textContent = text;
+  return el;
 }
