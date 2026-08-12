74f85928ed4d21aff31d15f31f661279de80f175
MiguelD <migueldoliveira142@gmail.com>
2026-08-12 07:50:45 +0100

    Refactor createEl function to accept attributes as an object

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,6 +1,9 @@
-export function createEl(el, cls, text) {
+export function createEl(el, {cls, text, attrs} = {}) {
   const element = document.createElement(el);
   if(cls) element.classList.add(cls);
   if(text) element.textContent = text;
+  if(attrs) Object.entries(attrs).forEach(([k, v]) => {
+    element.setAttribute(k, v);
+  })
   return element;
 }
